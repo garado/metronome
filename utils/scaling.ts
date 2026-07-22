a633186ccba7873785c2c7dd6ec10110bf5f8cfc
garado <alexisgarado@gmail.com>
2026-07-22 02:26:53 -0500

    chore: delete all old expo/react code in preparation for light-sdk migration

--- utils/scaling.ts
+++
@@ -1,7 +0,0 @@
-import { PixelRatio } from "react-native";
-
-const TARGET_DENSITY = 2.55;
-
-export const getDensityNormalization = () => TARGET_DENSITY / PixelRatio.get();
-
-export const n = (size: number) => size * getDensityNormalization();
